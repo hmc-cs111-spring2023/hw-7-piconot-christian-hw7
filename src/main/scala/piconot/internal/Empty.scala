b92168963396d19ce1be1ad08926aa9b9e0767e8
cvaldovinos <54999359+cvaldovinos@users.noreply.github.com>
2023-03-09 06:42:57 +0000

    Converting to more programming language syntax

--- src/main/scala/piconot/internal/Empty.scala
+++ src/main/scala/piconot/internal/Empty.scala
@@ -1,27 +1,62 @@
 package piconot.internal
 
 // A Picobot program that can fill an empty room
-object EmptyBot extends Picobor("resources/empty.txt") {
+object EmptyBot extends Picofun("resources/empty.txt") {
 
   // States 0 and 1: move to the top left
 
   // State 0: move left
-  (ifFacing('W') walls(isOpen('W'))) thenGo moveWestAndFace('W') // thenGo all the way to the left
-  (ifFacing('W') walls(isBlocked('W'))) thenGo stayHereAndFace('N') // can't thenGo left anymore, so try to thenGo up
+// if (facingWest) {
+//   if (isOpen('W') and isBlocked('S')) {
+//     moveWestAndFace('W')
+//   }
+// }
+
+// (ifFacingWest {
+//   (walls(isOpen('W') and isBlocked('S'))) {
+//     moveWestAndFace('W')
+//   }
+// })
+
+  (ifFacingWest {
+      isOpen('W')
+    }
+  ) {
+    moveWestAndFace('W')
+  } // thenGo all the way to the left
+
+  (ifFacingWest {isBlocked('W')}) {
+    stayHereAndFace('N')
+  } // can't thenGo left anymore, so try to thenGo up
 
   // State 1: move up
-  (ifFacing('N') walls(isOpen('N'))) thenGo moveNorthAndFace('N') // thenGo all the way to the top
-  (ifFacing('N') walls(isBlocked('N') and isOpen('S'))) thenGo moveSouthAndFace('S') // can't thenGo up any more, so try to thenGo down
+  (ifFacingNorth {isOpen('N')}) {
+    moveNorthAndFace('N')
+  } // thenGo all the way to the top
+  
+  (ifFacingNorth walls(isBlocked('N') and isOpen('S'))) {
+    moveSouthAndFace('S')
+  } // can't thenGo up any more, so try to thenGo down
 
   // States 2 and 3: fill from top to bottom, left to right
 
   // State 2: fill this column to the bottom
-  (ifFacing('S') walls(isOpen('S'))) thenGo moveSouthAndFace('S') // thenGo all the way to the bottom
-  (ifFacing('S') walls(isOpen('E') and isBlocked('S'))) thenGo moveEastAndFace('E') // can't thenGo down anymore, so try to thenGo right
+  (ifFacingSouth walls(isOpen('S'))) {
+    moveSouthAndFace('S')
+  } // thenGo all the way to the bottom
+  
+  (ifFacingSouth walls(isOpen('E') and isBlocked('S'))) {
+    moveEastAndFace('E')
+  } // can't thenGo down anymore, so try to thenGo right
 
   // State 3: fill this column to the top
-  (ifFacing('E') walls(isOpen('N'))) thenGo moveNorthAndFace('E') // thenGo all the way to the top
-  (ifFacing('E') walls(isBlocked('N') and isOpen('E'))) thenGo moveEastAndFace('S') // can't thenGo up anymore, so try to thenGo right
+  (ifFacingEast walls(isOpen('N'))) {
+    moveNorthAndFace('E')
+  } // thenGo all the way to the top
+  
+  (ifFacingEast walls(isBlocked('N') and isOpen('E'))) {
+    moveEastAndFace('S')
+  } // can't thenGo up anymore, so try to thenGo right
 
   run
 }
--- src/main/scala/piconot/internal/RightHand.scala
+++ src/main/scala/piconot/internal/RightHand.scala
@@ -2,29 +2,61 @@ package piconot.internal
 
 import scala.language.postfixOps
 
-object RightHand extends Picobor("resources/maze.txt") {
+object RightHand extends Picofun("resources/maze.txt") {
 
   // A picobot program that can solve a maze, using the right-hand rule
 
   // State 0: moving north
-  (ifFacing('N') walls(isOpen('E'))) thenGo moveEastAndFace('E') // we can thenGo right (to the east)
-  (ifFacing('N') walls(isOpen('N') and isBlocked('E'))) thenGo moveNorthAndFace('N') // we can't thenGo right, then try going forward (north)
-  (ifFacing('N') walls(isBlocked('N') and isBlocked('E'))) thenGo stayHereAndFace('S') // we can't thenGo right or forward, try south
+  (ifFacingNorth walls(isOpen('E'))) {
+    moveEastAndFace('E') 
+  } // we can thenGo right (to the east)
+
+  (ifFacingNorth walls(isOpen('N') and isBlocked('E'))) {
+    moveNorthAndFace('N') 
+  } // we can't thenGo right, then try going forward (north)
+  
+  (ifFacingNorth walls(isBlocked('N') and isBlocked('E'))) {
+    stayHereAndFace('S')
+  } // we can't thenGo right or forward, try south
 
   // State 1: moving east
-  (ifFacing('E') walls(isOpen('S'))) thenGo moveSouthAndFace('S') // we can thenGo right (to the south)
-  (ifFacing('E') walls(isOpen('E') and isBlocked('S'))) thenGo moveEastAndFace('E') // we can't thenGo right, then try going forward (east)
-  (ifFacing('E') walls(isBlocked('E') and isBlocked('S'))) thenGo stayHereAndFace('W') // we can't thenGo right or forward, try west
+  (ifFacingEast walls(isOpen('S'))) {
+    moveSouthAndFace('S')
+  } // we can thenGo right (to the south)
+  
+  (ifFacingEast walls(isOpen('E') and isBlocked('S'))) {
+    moveEastAndFace('E')
+  } // we can't thenGo right, then try going forward (east)
+
+  (ifFacingEast walls(isBlocked('E') and isBlocked('S'))) {
+    stayHereAndFace('W')
+  } // we can't thenGo right or forward, try west
 
   // State 2: moving west
-  (ifFacing('W') walls(isOpen('N'))) thenGo moveNorthAndFace('N') // we can thenGo right (to the north)
-  (ifFacing('W') walls(isBlocked('N') and isOpen('W'))) thenGo moveWestAndFace('W') // we can't thenGo right, then try going forward (west)
-  (ifFacing('W') walls(isBlocked('N') and isBlocked('W'))) thenGo stayHereAndFace('E') // we can't thenGo right or forward, try east
+  (ifFacingWest walls(isOpen('N'))) {
+    moveNorthAndFace('N')
+  } // we can thenGo right (to the north)
+
+  (ifFacingWest walls(isBlocked('N') and isOpen('W'))) {
+    moveWestAndFace('W')
+  } // we can't thenGo right, then try going forward (west)
+
+  (ifFacingWest walls(isBlocked('N') and isBlocked('W'))) {
+    stayHereAndFace('E')
+  } // we can't thenGo right or forward, try east
 
   // State 3: moving south
-  (ifFacing('S') walls(isOpen('W'))) thenGo moveWestAndFace('W') // we can thenGo right (to the west)
-  (ifFacing('S') walls(isBlocked('W') and isOpen('S'))) thenGo moveSouthAndFace('S') // we can't thenGo right, then try going forward (south)
-  (ifFacing('S') walls(isBlocked('W') and isBlocked('S'))) thenGo stayHereAndFace('N') // we can't thenGo right or forward, try north
+  (ifFacingSouth walls(isOpen('W'))) { 
+    moveWestAndFace('W') 
+  } // we can thenGo right (to the west)
+  
+  (ifFacingSouth walls(isBlocked('W') and isOpen('S'))) { 
+    moveSouthAndFace('S')
+  } // we can't thenGo right, then try going forward (south)
+
+  (ifFacingSouth walls(isBlocked('W') and isBlocked('S'))) {
+    stayHereAndFace('N')
+  } // we can't thenGo right or forward, try north
 
   run
 }
